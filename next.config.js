45e889f4c75be213fa1eccd8a47ec4468c621c00
carlostorresb <57723666+carlostorresb@users.noreply.github.com>
2026-04-03 23:47:16 -0500

    Fuerza UTF-8 en build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
 }
+
 module.exports = nextConfig
